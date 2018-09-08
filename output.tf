95040e889477b6925ea1b78e37554a4804fb6b08
Chaganti <rchaganti@TX717-0ASHTDH.local>
2018-09-08 18:50:55 -0500

    Added output

---
+++ output.tf
@@ -0,0 +1,31 @@
+# VPC
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = "${element(concat(aws_vpc.this.*.id, list("")), 0)}"
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = "${element(concat(aws_vpc.this.*.cidr_block, list("")), 0)}"
+}
+
+# Subnets
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = ["${aws_subnet.private.*.id}"]
+}
+
+output "private_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of private subnets"
+  value       = ["${aws_subnet.private.*.cidr_block}"]
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = ["${aws_subnet.public.*.id}"]
+}
+
+output "public_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of public subnets"
+  value       = ["${aws_subnet.public.*.cidr_block}"]
+}
