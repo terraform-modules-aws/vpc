be9092945b9c172c0c24fe7fde9c13034c9ab83e
Chaganti <rchaganti@TX717-0ASHTDH.local>
2018-09-08 18:44:57 -0500

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,80 @@
+######
+# VPC
+######
+resource "aws_vpc" "this" {
+   count = "${var.create_vpc ? 1 : 0}"
+
+   cidr_block            = "${var.cidr}"
+   instance_tenancy      = "${var.instance_tenancy}"
+   enable_dns_hostnames  = "${var.enable_dns_hostnames}"
+
+   tags = "${merge(map("Name", format("%s", var.name)), var.vpc_tags, var.tags)}"
+}
+
+################
+# Public subnet
+################
+resource "aws_subnet" "public" {
+  count = "${var.create_vpc && length(var.public_subnets) > 0 ? length(var.public_subnets) : 0}"
+
+  vpc_id                  = "${aws_vpc.this.id}"
+  cidr_block              = "${var.public_subnets[count.index]}"
+  availability_zone       = "${element(var.azs, count.index)}"
+  map_public_ip_on_launch = "${var.map_public_ip_on_launch}"
+
+  tags = "${merge(map("Name", format("%s-public-%s", var.name, element(var.azs, count.index))), var.public_subnet_tags, var.tags)}"
+}
+
+#################
+# Private subnet
+#################
+resource "aws_subnet" "private" {
+  count = "${var.create_vpc && length(var.private_subnets) > 0 ? length(var.private_subnets) : 0}"
+
+  vpc_id            = "${aws_vpc.this.id}"
+  cidr_block        = "${var.private_subnets[count.index]}"
+  availability_zone = "${element(var.azs, count.index)}"
+
+  tags = "${merge(map("Name", format("%s-private-%s", var.name, element(var.azs, count.index))), var.private_subnet_tags, var.tags)}"
+}
+
+###################
+# Internet Gateway
+###################
+resource "aws_internet_gateway" "this" {
+  count = "${var.create_vpc && length(var.public_subnets) > 0 ? 1 : 0}"
+
+  vpc_id = "${aws_vpc.this.id}"
+
+  tags = "${merge(map("Name", format("%s", var.name)), var.igw_tags, var.tags)}"
+}
+
+################
+# Publiс routes
+################
+resource "aws_route_table" "public" {
+  count = "${var.create_vpc && length(var.public_subnets) > 0 ? 1 : 0}"
+
+  vpc_id = "${aws_vpc.this.id}"
+
+  tags = "${merge(map("Name", format("%s-public", var.name)), var.public_route_table_tags, var.tags)}"
+}
+
+resource "aws_route" "public_internet_gateway" {
+  count = "${var.create_vpc && length(var.public_subnets) > 0 ? 1 : 0}"
+
+  route_table_id         = "${aws_route_table.public.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.this.id}"
+
+  timeouts {
+    create = "5m"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = "${var.create_vpc && length(var.public_subnets) > 0 ? length(var.public_subnets) : 0}"
+
+  subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
+  route_table_id = "${aws_route_table.public.id}"
+}
---
+++ variables.tf
@@ -0,0 +1,69 @@
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  default     = ""
+}
+
+variable "create_vpc" {
+  description = "Controls if VPC should be created (it affects almost all resources)"
+  default     = true
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
+  default     = "0.0.0.0/0"
+}
+
+variable "instance_tenancy" {
+  description = "A tenancy option for instances launched into the VPC"
+  default     = "default"
+}
+
+variable "enable_dns_hostnames" {
+  description = "Should be true to enable DNS hostnames in the VPC"
+  default     = false
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  default     = {}
+}
+
+variable "vpc_tags" {
+  description = "Additional tags for the VPC"
+  default     = {}
+}
+
+variable "public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  default     = []
+}
+
+variable "public_subnet_tags" {
+  description = "Additional tags for the public subnets"
+  default     = {}
+}
+
+variable "private_subnets" {
+  description = "A list of private subnets inside the VPC"
+  default     = []
+}
+
+variable "private_subnet_tags" {
+  description = "Additional tags for the private subnets"
+  default     = {}
+}
+
+variable "azs" {
+  description = "A list of availability zones in the region"
+  default     = []
+}
+
+variable "map_public_ip_on_launch" {
+  description = "Should be false if you do not want to auto-assign public IP on launch"
+  default     = true
+}
+
+variable "igw_tags" {
+  description = "Additional tags for the internet gateway"
+  default     = {}
+}
